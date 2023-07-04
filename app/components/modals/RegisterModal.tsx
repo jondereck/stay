b62e6df5e50d97d248cfd8cad34c4687bb87036c
jondereck <jonderecknifas@gmail.com>
2023-07-04 11:35:06 +0800

    first commit

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,176 @@
+"use client";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm, Validate } from "react-hook-form";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
+import Heading from "./Heading";
+import Input from "../Input";
+import { toast } from "react-hot-toast";
+import Button from "../Button";
+import useLoginModal from "@/app/hooks/useLoginModal";
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      repeatPass: "",
+    },
+  });
+
+  const validateMatchPassword: Validate<string, FieldValues> = (
+    value,
+    formValues
+  ) => {
+    const password = formValues.password;
+    return value === password || "Passwords do not match";
+  };
+
+  const onSumbit: SubmitHandler<FieldValues> = (data) => {
+    const password = watch("password"); // Access the value of the "password" field
+    const repeatPass = watch("repeatPass"); // Access the value of the "repeatPass" field
+
+    if (password !== repeatPass) {
+      // Handle password mismatch error
+      setIsLoading(false);
+      toast.error("Something");
+      return;
+    }
+    setIsLoading(true);
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        toast.error("Something went wrong");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading
+        title="Welcome to Stay"
+        subtitle="Your staycation that feels home"
+      />
+
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+
+      <Input
+        id="repeatPass"
+        label="Repeat password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        validate={validateMatchPassword}
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+
+      <div
+        className="
+        text-neutral-500
+          text-center
+          mt-4
+          font-light
+        "
+      >
+        <div className="
+          flex 
+          flex-row 
+          justify-center
+          items-center 
+          gap-2"
+          >
+            <div>
+                Aready have an Acount?
+            </div>
+            <div 
+            onClick={() => {registerModal.onClose, loginModal.onOpen}}
+            className="
+              text-neutral-800
+              cursor-pointer
+              hover:underline"
+              >
+                Login
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onSubmit={handleSubmit(onSumbit)}
+      onClose={registerModal.onClose}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+};
+
+export default RegisterModal;
---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,118 @@
+'use client'
+import { User } from "@prisma/client"
+import { AiOutlineMenu } from 'react-icons/ai';
+import Avatar from '../Avatar';
+import { useCallback, useState } from 'react';
+import MenuItem from './MenuItem';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
+import { signOut } from "next-auth/react"
+
+
+interface UserMenuProps {
+  currentUser?: User | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({
+  currentUser,
+}) => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(()=> {
+    setIsOpen((value) => !value);
+  },[]);
+
+  return (  
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div 
+        onClick={() => {} } 
+        className="hidden text-sm 
+        md:block font-semibold py-3 px-4 rounded-full 
+        hover:bg-neutral-100
+        transition cursor-pointer">Stay your own home.</div>
+    
+      <div 
+      onClick={toggleOpen}
+      className="p-4 
+        md:py-1 
+        md:px-2 
+        border-[1px] 
+        border-neutral-100 
+        flex
+        flex-row
+        items-center 
+        gap-3 
+        rounded-full 
+        cursor-pointer 
+        hover:shadow-md 
+        transition">
+        <AiOutlineMenu/>
+      </div>
+        <div className='hidden md:block'>
+          <Avatar/>
+       
+      </div>
+      </div> 
+
+      {isOpen && (
+        <div
+        className='
+        absolute
+        rounded-xl
+        shawdow-md
+        w-[40vw]
+        md:w-3/4
+        bg-white
+        overflow-hidden
+        right-0
+        top-12
+        text-sm'>
+          <div className='flex flex-col cursor-pointer'>
+          {currentUser ? (
+            <>
+              <MenuItem 
+              onClick={loginModal.onOpen}
+              label='My trips'/>
+
+              <MenuItem
+              onClick={registerModal.onOpen}
+              label='My favorites'/>
+               <MenuItem
+              onClick={registerModal.onOpen}
+              label='My reservations'/>
+               <MenuItem
+              onClick={registerModal.onOpen}
+              label='My properties'/>
+               <MenuItem
+              onClick={registerModal.onOpen}
+              label='My home'/>
+               <MenuItem
+              onClick={() => signOut()}
+              label='Logout'/>
+            </>
+
+          ) : (
+            <>
+              <MenuItem 
+              onClick={loginModal.onOpen}
+              label='Login'/>
+
+              <MenuItem
+              onClick={registerModal.onOpen}
+              label='Register'/>
+            </>
+          )
+      }
+            
+            
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+ 
+export default UserMenu;
